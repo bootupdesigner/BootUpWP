d6dba53019dd54a1d481e28c7c6b6a2a661312d7
bootupdesigner <bootup.itinfo@gmail.com>
2021-04-09 21:50:07 -0400

    I converted unnecesary html to JavaScript

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,8 +12,52 @@ import theofficialgluelogo from "../images/theofficialgluelogo.png";
 import frannysfannyandmorelogo from "../images/frannysfannyandmorelogo.png";
 import kingkasslogo from "../images/kingkasslogo.png";
 
-const Portfolio = () => {
-  return (
+const Portfolio = (props) => {
+  const websites = [
+    {
+      logo: kingkasslogo,
+      alt: "King Kass Logo",
+      title: "King Kass",
+      text: "King Kass is a mink lash store and blog website. King Kass is your go-to for lash and make-up tips. Purchase your lashes and learn how to install them from King Kass herself.",
+      link: "https://www.kingkass.com/"
+    },
+    {
+      logo: theofficialgluelogo,
+      alt: "GLUE LLC logo",
+      title: "Glue LLC",
+      text: "The Official Glue website was designed as an event page for GLUE LLC's one week webinar event. The website provides guests with information about the events and how they can attend.",
+      link: "https://www.theofficialglue.com/"
+    },
+    {
+      logo: ctyslogo,
+      alt: "cheaper than your stylist logo",
+      title: "Cheaper Than Your Stylist",
+      text: "Cheaper Than Your Stylist started as a store where you could purchase your lashes for less. Since opening the online store has evolved into much more, providing small business resources now as well.",
+      link: "https://www.ctys.store/"
+    },
+    {
+      logo: trifinancialserviceslogo,
+      alt: "Tri Financial Services logo",
+      title: "Tri Financial Services",
+      text: "Tri Financial Services website's design was a revamp for us. Providing financial services, this informational website was designed to help plan a financially successful future for you and your family.",
+      link: "https://www.trifinancialservices.com/"
+    },
+    {
+      logo: clutchcouturelogo,
+      alt: "Clutch Couture LLC logo",
+      title: "Clutch Couture LLC ",
+      text: "Clutch Couture is an experience like no other; This website allows you to plan your own paint party with all the materials included. Book a date, make a deposit and invite your friends. This website has everything.",
+      link: "https://www.clutch-couture.com/"
+    },
+    {
+      logo:frannysfannyandmorelogo,
+      alt:"Franny's Fanny and More logo",
+      title: "Franny's Fanny and More",
+      text: "Franny's Fanny and More was inspired by Breast Cancer survivor, Franny. This website is a testament to Franny's journey through the fight for her life and her son's autism.",
+      link: "https://www.frannysfannyandmore.online/"
+    }
+  ];
+  return(
     <div>
       <div className="page-description">
         <h1>Portfolio</h1>
@@ -23,75 +67,28 @@ const Portfolio = () => {
       <h2>We design websites to fit your business needs!</h2>
       <span>Here's a few of our completed projects</span>
       </div>
-      <CardDeck style={{ margin: "5px 10px" }}>
-        <Card>
+        
+      {/* this is the card deck */}
+      {websites.map((website) => {
+        return(
+          <CardDeck style={{ margin: "5px 10px" }}>
+        
+            <Card>
           <Card.Img
             variant="top"
-            src={theofficialgluelogo}
-            alt="GLUE LLC logo"
+            src={website.logo}
+            alt={website.alt}
             width="15vw"
           />
           <Card.Body>
-            <Card.Title>GLUE LLC</Card.Title>
+            <Card.Title>{website.title}</Card.Title>
             <Card.Text>
-              The Official Glue website was designed as an event page for GLUE
-              LLC's one week webinar event. The website provides guests with
-              information about the events and how they can attend.
+              {website.text}
             </Card.Text>
             <Button
               variant="primary"
               size="sm"
-              href="https://www.theofficialglue.com/"
-              target="_blank"
-            >
-              View Website
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={ctyslogo}
-            alt="cheaper than your stylist logo"
-            width="15vw"
-          />
-          <Card.Body>
-            <Card.Title>Cheaper Than Your Stylist</Card.Title>
-            <Card.Text>
-              Cheaper Than Your Stylist started as a store where you could
-              purchase your lashes for less. Since opening the online store has
-              evolved into much more, providing small business resources now as
-              well.
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              href="https://www.ctys.store/"
-              target="_blank"
-            >
-              View Website
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={trifinancialserviceslogo}
-            alt="clutch couturellc logo"
-            width="15vw"
-          />
-          <Card.Body>
-            <Card.Title>Tri Financial Services</Card.Title>
-            <Card.Text>
-              Tri Financial Services website's design was a revamp for us.
-              Providing financial services, this informational website was
-              designed to help plan a financially successful future for you and
-              your family.
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              href="https://www.trifinancialservices.com/"
+              href={website.link}
               target="_blank"
             >
               View Website
@@ -99,83 +96,9 @@ const Portfolio = () => {
           </Card.Body>
         </Card>
       </CardDeck>
-      <br />
-      {/* this is the second card deck */}
-      <CardDeck style={{ margin: "5px 10px" }}>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={clutchcouturelogo}
-            alt="Clutch Couture LLC logo"
-            width="15vw"
-          />
-          <Card.Body>
-            <Card.Title>Clutch Couture LLC</Card.Title>
-            <Card.Text>
-              Clutch Couture is an experience like no other; This website allows
-              you to plan your own paint party with all the materials included.
-              Book a date, make a deposit and invite your friends. This website
-              has everything.
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              href="https://www.clutch-couture.com/"
-              target="_blank"
-            >
-              View Website
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={frannysfannyandmorelogo}
-            alt="frannys fanny and more logo"
-            width="15vw"
-          />
-          <Card.Body>
-            <Card.Title>Franny's Fanny and More</Card.Title>
-            <Card.Text>
-              Franny's Fanny and More was inspired by Breast Cancer survivor,
-              Franny. This website is a testament to Franny's journey through
-              the fight for her life and her son's autism.
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              href="https://www.frannysfannyandmore.online/"
-              target="_blank"
-            >
-              View Website
-            </Button>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img
-            variant="top"
-            src={kingkasslogo}
-            alt="King Kass Logo"
-            width="15vw"
-          />
-          <Card.Body>
-            <Card.Title>King Kass</Card.Title>
-            <Card.Text>
-              King Kass is a mink lash store and blog website. King Kass is your
-              go-to for lash and make-up tips. Purchase your lashes and learn
-              how to install them from King Kass herself.
-            </Card.Text>
-            <Button
-              variant="primary"
-              size="sm"
-              href="https://www.kingkass.com/"
-              target="_blank"
-            >
-              View Website
-            </Button>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+        )
+      }
+      )}
       <ServiceLink />
     </div>
   );
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,6 +8,18 @@ import { Button, Card, Container, Row, Col} from "react-bootstrap";
 
 
 const Services = () => {
+  const offer = [
+    {
+      blog: "Website Designs",
+      description: "Boot Up Web Professionals design WordPress websites that are 100% responsive for Desktop, Laptop, Tablet, and Mobile. Your website comes with a full e-commerce integration including:" 
+    },{
+      blog: "SEO Improvements",
+      description: "Your SEO results are populated by more than your website alone, developing your presence on the internet improves your SEO. Our E-Commerce Design Specialist..." 
+    },{
+      blog: "Hosting and Maintenance",
+      description: "Publishing your website to the internet requires a hosting service. Great news our premium hosting service also comes with a free SSL Certificate that keeps your users information safe..." 
+    }
+  ];
   return (
     <div>
       <div className="page-description">
@@ -18,33 +30,23 @@ const Services = () => {
         <h3>Business Startups</h3>
         <p style={{padding: "0px 10vw 0px 10vw"}}>Boot Up Web Professionaionals offers a number of services to help jump start you business or creative idea. We'll provide you with Limitied Liability Company filings (LLC), Employer ID Number(EIN), Vendors Tax ID, Vendors List, Logos, and a <i>WEBSITE!</i></p>
       </div>
-      {/* card number one */}
-      <Card className="service-card">
+      {/* theses are service cards */}
+    {offer.map((service)=> {
+      return(
+        <Card className="service-card">
   <Card.Header as="h5">Services</Card.Header>
   <Card.Body>
-    <Card.Title>Website Designs</Card.Title>
+    <Card.Title>{service.blog}</Card.Title>
     <Card.Text>
-    Boot Up Web Professionals design WordPress websites that are 100% responsive for Desktop, Laptop, Tablet, and Mobile. Your website comes with a full e-commerce integration including:
+    {service.description} 
     </Card.Text>
     <Button variant="outline-dark">
-       <Link to="/WebsiteDesigns">Learn More</Link>
+       <Link to="/Hosting">Learn More</Link>
         </Button>
   </Card.Body>
 </Card>
-{/* card number two */}
-<Card className="service-card">
-  <Card.Header as="h5">Services</Card.Header>
-  <Card.Body>
-    <Card.Title>SEO Improvements</Card.Title>
-    <Card.Text>
-    Your SEO results are populated by more than your website alone, developing your presence on the internet improves your SEO. Our E-Commerce Design Specialist...
-    </Card.Text>
-    <Button variant="outline-dark">
-       <Link to="/SeoImprovements">Learn More</Link>
-        </Button>
-  </Card.Body>
-</Card>
-{/* card three starts here */}
+      )
+    })}
 <Card className="service-card">
   <Card.Header as="h5">Services</Card.Header>
   <Card.Body>
